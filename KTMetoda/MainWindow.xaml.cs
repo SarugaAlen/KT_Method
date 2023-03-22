da18ae73f204ac45cdf4de917e9392420c6233e6
Alen Šaruga <66218970+SarugaAlen@users.noreply.github.com>
2023-03-22 19:56:24 +0100

    Add project files.

---
+++ KTMetoda/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KTMetoda
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> Alternative { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Alternative = new ObservableCollection<string>();
+            DataContext = this;
+        }
+
+        private void DodajAlternativo_Click(object sender, RoutedEventArgs e)
+        {
+            string alternativa = VnosAlternative.Text;
+            Alternative.Add(alternativa);
+        }
+
+        private void IzbrisiAlternativo_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedAlternative = AlternativaListBox.SelectedItem as string;
+            if (AlternativaListBox.Items.Count == 0)
+            {
+                MessageBox.Show("Napak: Seznam  je prazen!", "Napaka", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else
+            {
+                if (selectedAlternative != null)
+                {
+                    Alternative.Remove(selectedAlternative);
+                }
+            }           
+        }
+        private void NaprejParametre_Click(object sender, RoutedEventArgs e)
+        {
+            if (Alternative.Count == 0)
+            {
+                MessageBox.Show("Napaka: Seznam alternativ je prazen!", "Napaka", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            ParametriWindow parametriWindow = new ParametriWindow(Alternative);
+            parametriWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ KTMetoda/ParametriWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KTMetoda
+{
+    /// <summary>
+    /// Interaction logic for ParametriWindow.xaml
+    /// </summary>
+    public partial class ParametriWindow : Window
+    {
+        public ObservableCollection<string> Alternative;
+        public ParametriWindow(ObservableCollection<string> Alternative)
+        {
+            InitializeComponent();
+            this.Alternative = Alternative;
+            DataContext = this;
+        }
+
+        private void DodajParameter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IzbrisParameter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NaprejOceno_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
