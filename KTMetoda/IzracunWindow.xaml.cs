6fd6ef0a2bd8e8bcb7c06602125df408c80d283a
Alen Šaruga <66218970+SarugaAlen@users.noreply.github.com>
2023-03-24 18:38:42 +0100

    midstage

--- KTMetoda/IzracunWindow.xaml.cs
+++ KTMetoda/IzracunWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace KTMetoda
     /// <summary>
     /// Interaction logic for IzracunWindow.xaml
     /// </summary>
+    /// 
     public partial class IzracunWindow : Window
     {
         public ObservableCollection<string> Alternative;
@@ -28,6 +29,49 @@ namespace KTMetoda
             InitializeComponent();
             this.Alternative = Alternative;
             this.Parametri = Parametri;
+
+            //DataGridUtezi.ItemsSource = Parametri;
+            var parametriColumn = new DataGridTemplateColumn
+            {
+                Header = "Parametri",
+                CellTemplate = new DataTemplate(typeof(ListBox))
+            };
+
+            //var itemTemplate = new DataTemplate();
+
+            //var stackPanelFactory = new FrameworkElementFactory(typeof(StackPanel));
+            //stackPanelFactory.SetValue(StackPanel.OrientationProperty, Orientation.Horizontal);
+
+            //var keyTextBlockFactory = new FrameworkElementFactory(typeof(TextBlock));
+            //keyTextBlockFactory.SetBinding(TextBlock.TextProperty, new Binding("Key"));
+            //stackPanelFactory.AppendChild(keyTextBlockFactory);
+
+            //var valueTextBlockFactory = new FrameworkElementFactory(typeof(TextBlock));
+            //valueTextBlockFactory.SetBinding(TextBlock.TextProperty, new Binding("Value"));
+            //stackPanelFactory.AppendChild(valueTextBlockFactory);
+
+            //itemTemplate.VisualTree = stackPanelFactory;
+
+            //var listBoxFactory = new FrameworkElementFactory(typeof(ListBox));
+            //listBoxFactory.SetValue(ListBox.ItemTemplateProperty, itemTemplate);
+            //listBoxFactory.SetBinding(ListBox.ItemsSourceProperty, new Binding());
+
+            //parametriColumn.CellTemplate = new DataTemplate();
+            //parametriColumn.CellTemplate.VisualTree = listBoxFactory;
+
+            //parametriColumn.SetValue(DataGrid.ItemsSourceProperty, Parametri);
+
+            //myDataGrid.Columns.Insert(0, parametriColumn);
+
+            foreach (string alternative in Alternative)
+            {
+                var column = new DataGridTextColumn
+                {
+                    Header = alternative,
+                    Binding = new Binding(string.Format("[{0}]", alternative))
+                };
+                myDataGrid.Columns.Add(column);
+            }
         }
     }
 }
