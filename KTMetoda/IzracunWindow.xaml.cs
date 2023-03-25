d397a9f1e76e99a274d48d74755ab05e35f45108
Alen Šaruga <66218970+SarugaAlen@users.noreply.github.com>
2023-03-25 11:31:57 +0100

    cell data input works

--- KTMetoda/IzracunWindow.xaml.cs
+++ KTMetoda/IzracunWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace KTMetoda
     {
         public ObservableCollection<string> Alternative { get; set; }
         public ObservableCollection<Parameter> Parametri { get; set; }
-        public DataTable Data { get; set; }
+        public List<List<int>> Data { get; set; } = new List<List<int>>();
 
 
         public IzracunWindow(ObservableCollection<string> Alternative, ObservableCollection<Parameter> Parametri)
@@ -40,30 +40,64 @@ namespace KTMetoda
             int steviloVrst = Parametri.Count;
             int steviloStolpcev = Alternative.Count;
 
-            //List<int> data = new List<int>();
-            List<int> data = new List<int>();
-            ////// initialize the data source with the desired number of objects
-            //for (int i = 0; i < steviloVrst; i++)
-            //{
-            //    data.Add(0);
-            //}
-
-            //// set the data source for the DataGrid
-            //GridAlternative.ItemsSource = data;
-
-            foreach (string alternativa in Alternative)
+            for (int i = 0; i < steviloStolpcev; i++)
             {
-                int item = 0;
-                data.Add(item);
+                Data.Add(new List<int>());
+            }
 
+            for (int i = 0; i < steviloStolpcev; i++)
+            {
                 DataGridTextColumn column = new DataGridTextColumn();
-                column.Header = alternativa;
-                column.Binding = new Binding(alternativa);
+                column.Header = Alternative[i];
                 column.Width = 200;
                 column.IsReadOnly = false;
+                column.Binding = new Binding($"Data[{i}]"); // Bind to the entire list for the corresponding row index
                 GridAlternative.Columns.Add(column);
             }
-            GridAlternative.ItemsSource = data;
+
+            GridAlternative.ItemsSource = Data;
+            GridAlternative.AutoGenerateColumns = false;
+            GridAlternative.CanUserAddRows = false;
+
+            GridAlternative.CellEditEnding += GridAlternative_CellEditEnding;
+        }
+        private void GridAlternative_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            string newValue = ((TextBox)e.EditingElement).Text;
+
+            // Get the row and column of the edited cell
+            int rowIndex = e.Row.GetIndex();
+            int columnIndex = e.Column.DisplayIndex;
+
+            // Convert the new value to an integer
+            int intValue;
+            if (int.TryParse(newValue, out intValue))
+            {
+                // If the list for this column doesn't have enough items (rows), add them
+                while (Data[columnIndex].Count <= rowIndex)
+                {
+                    Data[columnIndex].Add(0);
+                }
+
+                // Update the corresponding item in the Data list with the new value
+                Data[columnIndex][rowIndex] = intValue;
+
+                // Cancel the edit so that the new value is not discarded
+                e.Cancel = true;
+
+                // Update the cell's value to the new value
+                var cell = e.Column.GetCellContent(e.Row);
+                if (cell != null)
+                {
+                    ((TextBox)cell).Text = newValue;
+                }
+            }
+            else
+            {
+                // If the user entered an invalid value, display a message and reset the cell's value
+                MessageBox.Show("Invalid input. Please enter an integer value.");
+                ((TextBox)e.EditingElement).Text = Data[columnIndex][rowIndex].ToString();
+            }
         }
     }
 }
